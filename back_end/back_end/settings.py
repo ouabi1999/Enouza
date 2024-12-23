d79f21ce60873a88dd35aa7660896f7fd6603a42
ouabi1999 <ouabiabdessamad@gmail.com>
2024-12-23 03:56:24 +0100

    add some configrations for deploying1

--- back_end/back_end/settings.py
+++ back_end/back_end/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["f93b-197-147-124-175.ngrok-free.app", "127.0.0.1"]
+ALLOWED_HOSTS =  os.getenv("ALLOWED_HOSTS")
 
 
 # Application definition
