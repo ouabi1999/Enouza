e9fb105c07fd92fef38e88fc74e715bbc0413761
ouabi1999 <ouabiabdessamad@gmail.com>
2024-12-23 05:46:38 +0100

    update static rout to loud static front_end file

--- front_end/common/baseUrl.js
+++ front_end/common/baseUrl.js
@@ -2,7 +2,7 @@ import axios from   "axios"
 import { ACCESS_TOKEN } from './constant';
 
 const ApiInstance = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api/',
+    baseURL: 'https://enouzaserver.onrender.com/api/',
     timeout: 50000,
     
    
