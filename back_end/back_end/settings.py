8176d4ccd159d4e629938612dac021ae38e791bd
ouabi1999 <ouabiabdessamad@gmail.com>
2024-12-24 01:37:12 +0100

    change sqlite3 to postgressql

--- back_end/back_end/settings.py
+++ back_end/back_end/settings.py
@@ -122,9 +122,14 @@ WSGI_APPLICATION = 'back_end.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'Enouzadb.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
+
 }
 
 
