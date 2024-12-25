0611068abf136d21e08df6aa036d27e1d9da287f
ouabi1999 <ouabiabdessamad@gmail.com>
2024-12-25 02:09:34 +0100

    change databes to sqlite3

--- back_end/back_end/settings.py
+++ back_end/back_end/settings.py
@@ -121,12 +121,18 @@ WSGI_APPLICATION = 'back_end.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
         default= os.getenv("DATABASE_URL"),
         conn_max_age=600
     )
+}"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
