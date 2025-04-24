97d526cc8ec13d0331cf6f128de03419e38a3e19
ouabi1999 <ouabiabdessamad@gmail.com>
2025-04-24 23:20:23 +0200

    make some settings changes for deploying the app to digitalocean

--- back_end/build.sh
+++ back_end/build.sh
@@ -5,8 +5,6 @@ set -o errexit
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
-# Convert static asset files
-python manage.py collectstatic
 
 # Apply any outstanding database migrations
 python manage.py makemigrations
