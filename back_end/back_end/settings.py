154a84c69ca666028e634bd00d900c72dacd5a84
ouabi1999 <ouabiabdessamad@gmail.com>
2025-04-26 18:30:26 +0200

    fix some issues 8

--- back_end/back_end/settings.py
+++ back_end/back_end/settings.py
@@ -97,7 +97,7 @@ else:
     if os.getenv("DATABASE_URL", None) is None:
         raise Exception("DATABASE_URL environment variable not defined")
     DATABASES = {
-        "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
+        "default": dj_database_url.parse(os.getenv("DATABASE_URL")),
     }
 
 
