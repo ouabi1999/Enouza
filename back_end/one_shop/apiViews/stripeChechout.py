934c050ca1b03aa3885aad478e703caf0c2f281f
ouabi1999 <ouabiabdessamad@gmail.com>
2025-10-12 18:45:49 +0200

    fix an error

--- back_end/one_shop/apiViews/stripeChechout.py
+++ back_end/one_shop/apiViews/stripeChechout.py
@@ -22,7 +22,7 @@ env = environ.Env(
 
 
 stripe.api_key = env("STRIPE_SECRET_KEY")
-YOUR_DOMAIN = env("SERVER_HOST")
+YOUR_DOMAIN = env("CLIENT_HOST")
 
 endpoint_secret = env("END_POINT_SECRET") # Add your endpoint secret to Django settings
 
