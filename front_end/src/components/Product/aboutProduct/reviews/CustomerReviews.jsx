ab8de8b21553d65350e45c9ec2b557ead0ba64cd
ouabi1999 <ouabiabdessamad@gmail.com>
2025-10-26 15:45:41 +0100

    add fi

--- front_end/src/components/Product/aboutProduct/reviews/CustomerReviews.jsx
+++ front_end/src/components/Product/aboutProduct/reviews/CustomerReviews.jsx
@@ -63,9 +63,9 @@ function CustomerReviews(props) {
                       );
                     })}
                   </div>
-                  {/*<div>
+                  <div>
                     <span style={{ marginLeft: "4px" , fontSize:"16px"}}> {rate.user?.firstName.slice(0, 2) + "***" + rate.user?.firstName.slice(4, 6)}</span>
-                  </div>*/}
+                  </div>
                   <div style={{ marginLeft: "4px" , fontSize:"14px"}}>
                     <p>{rate.review.text}</p>
                   </div>
@@ -223,7 +223,7 @@ const Container = styled.div`
 `;
 const ChildContainer = styled.div`
     display: grid;
-    grid-template-columns: auto auto;
+    grid-template-columns: auto;
     gap: 10px;
     margin-top: 15px;
 
