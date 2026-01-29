9d8fa51de550d23f2627e346873f19ff0eaac60a
ouabi1999 <ouabiabdessamad@gmail.com>
2026-01-29 23:03:45 +0100

    fix remove image detalis

--- front_end/src/components/Product/home/Products.jsx
+++ front_end/src/components/Product/home/Products.jsx
@@ -74,8 +74,7 @@ export default Products;
 const ProductContainer = styled.div`
   width:100%;
   min-width: 300px;
-  max-height: 400px;
-  max-width: 400px;
+
   font-family: 'Arial Narrow', Arial, sans-serif;
 
   .grid-container {
@@ -86,8 +85,9 @@ const ProductContainer = styled.div`
     place-items: ${(props) => props.place_items || 'center'} ;
 
     img {
-      max-height: 400px;
-      max-width: 400px;
+      width: 100%;
+      height: 100%;
+     
       object-fit: cover;
       background-color: #fff;
     }
