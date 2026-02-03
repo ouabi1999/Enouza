6847355c968027b867a31853488210b76daa49fd
ouabi1999 <ouabiabdessamad@gmail.com>
2026-02-04 00:50:00 +0100

    fix some design issues

--- front_end/src/pages/ProductDetailsPage.jsx
+++ front_end/src/pages/ProductDetailsPage.jsx
@@ -52,7 +52,7 @@ function ProductDetailsPage({ setRetry, retry }) {
   const dispatch = useDispatch();
   const index = uuidv4();
 
-  useLayoutEffect(() => {
+  useEffect(() => {
 
       dispatch(getProductDetails(params.id))
       //window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
