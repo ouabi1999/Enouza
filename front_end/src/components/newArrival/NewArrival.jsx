c3e2f72120cc369537cbf72c19de1e8e083e74e4
ouabi1999 <ouabiabdessamad@gmail.com>
2026-09-16 01:34:03 +0200

    make a save label and new or bestsellers in the same position

--- front_end/src/components/newArrival/NewArrival.jsx
+++ front_end/src/components/newArrival/NewArrival.jsx
@@ -12,7 +12,7 @@ import "swiper/css/navigation";
 import { Save } from "lucide-react";
 import { tr } from "framer-motion/client";
 
-function NewArrival({ products = [], name, label, loop = true }) {
+function NewArrival({ products = [], name, label, loop}) {
   const { t, i18n } = useTranslation();
   const prevRef = useRef(null);
   const nextRef = useRef(null);
