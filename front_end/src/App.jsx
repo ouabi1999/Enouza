0ac89fddd056df46a71cae946a9dff4e8609dbef
ouabi1999 <ouabiabdessamad@gmail.com>
2026-09-21 19:37:38 +0200

    fix some isuue

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -25,7 +25,8 @@ import "./App.css";
 import HomePage from "./pages/HomePage.jsx";
 
 import { getUser } from "./features/authSlice";
-
+import { getProduct } from "./features/productSlice";
+import { getDisplayInfo } from "./features/DisplaySlice.js";
 
 import { useTranslation } from "react-i18next";
 
@@ -256,7 +257,10 @@ function App() {
   useEffect(() => {
     dispatch(getUser());
 
-  }, [retry]);
+    dispatch(getProduct());
+
+    dispatch(getDisplayInfo());
+  }, [dispatch, retry]);
 
 
   /*
